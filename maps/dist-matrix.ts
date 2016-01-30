659285608a71a6e347c6a6d6d851dbe4b5765b1a
Richard Sun <rsun@media.ucla.edu>
2016-01-29 21:54:24 -0800

    read apartment list

--- maps/dist-matrix.ts
+++ maps/dist-matrix.ts
@@ -1,6 +1,15 @@
 /// <reference path="node.d.ts" />
 import * as http from 'http';
 import * as querystring from 'querystring';
+import * as fs from 'fs';
+
+interface Apartment {
+  id: string;
+  address: string;
+}
+
+var contents: Buffer = fs.readFileSync('apartments.json');
+var apartments: Apartment[] = JSON.parse(contents.toString());
 
 var api_key: string = 'AIzaSyD6XhAXWqX9WI49xvmY-YY1pMaKRI2qKB0';
 var endpt: string = 'https://maps.googleapis.com/maps/api/distancematrix/json';
