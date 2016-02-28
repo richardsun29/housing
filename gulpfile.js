cab0fb9d6e18c5c5e1d43308c3557da46e4e1c14
Richard Sun <rsun@media.ucla.edu>
2016-02-28 02:31:51 -0800

    Greatly slim down www/ size
    - gulpfile.js: don't generate unminified css
    - www/fonts/: moved from lib/ionic/release/fonts/
    - lib/: delete all bloat, keep only used .min.js files
    - .gitignore: no longer ignore www/lib/, removes bower dependency
    - README.md: updated to no longer require `bower install`
    - index.html, web.html: change script src's to new lib/ files

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
     .pipe(sass())
     .on('error', sass.logError)
-    .pipe(gulp.dest('./www/css/'))
+    //.pipe(gulp.dest('./www/css/')) // save unminified copy
     .pipe(minifyCss({
       keepSpecialComments: 0
     }))
